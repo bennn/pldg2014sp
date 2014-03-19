1937f3125d37dc3ed84db9326ca7552aa430ec7b
Ben <ben@rentenna.com>
2014-03-19 04:10:30 -0400

    ** init

---
+++ presentation/introduction.rkt
@@ -0,0 +1,4 @@
+(slide
+  #:title "From Parametricity to Conservation Laws, via Noether's Theorem"
+  (t "Robert Atkey")
+  (tt "bob.atkey@gmail.com"))
---
+++ presentation/main.rkt
@@ -0,0 +1,9 @@
+#lang slideshow
+
+(require introduction)
+(require parametricity)
+(require atkey-intro)
+(require atkey-intuition)
+(require atkey-examples)
+(require atkey-formalisms)
+(require conclusion)
