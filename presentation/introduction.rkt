43f68364decedad50b0595110a9c7d890596076e
Ben <ben@rentenna.com>
2014-03-20 21:47:50 -0400

    [show] updated intro, it's just one slide

--- presentation/introduction.rkt
+++ presentation/introduction.rkt
@@ -1,4 +1,10 @@
 (slide
   #:title "From Parametricity to Conservation Laws, via Noether's Theorem"
-  (t "Robert Atkey")
-  (tt "bob.atkey@gmail.com"))
+ (vc-append (* gap-size 8)
+            (vc-append (* gap-size 1)
+                       (t "Robert Atkey")
+                       (tt "bob.atkey@gmail.com"))
+            (vc-append (* gap-size 1)
+                       (para "Presented by Ben " (tt "{ Carriel, Greenman }"))
+                       (t "April 7, 2014"))))
+
