cf283417362a4adb65229118cbe56b4a2a88d7d4
Ben <ben@rentenna.com>
2014-03-20 21:48:51 -0400

    [show] outline of Atkey summary, needs CONTENT

---
+++ presentation/atkey.rkt
@@ -0,0 +1,35 @@
+(slide (t "PART III: ATKEY"))
+
+(slide #:title "Atkey's Observation"
+   (para "Question: Can we formalize the invariance of physical systems?")
+   'next
+   (item "Noether: physical systems are invariant under change of coordinates")
+   'next
+   (item "Reynolds: polymorphic functions are invariant under change of data representation.")
+   'next
+   (para "Idea: use a type system to ensure invariance properties."))
+
+;; Give a physical system, it's equation, and the equation of its type.
+;; Build intuition about the type.
+;; Can use example of mass-on-a-spring spelled out in section 1 or any of the section 5 examples
+(slide #:title "Atkey (2014)"
+   (para (todo "mass on a spring revisited. Give equation. Give type."))
+   (para (todo "Step through the type very slowly")))
+
+(slide #:title "Atkey (2014)"
+   (para (todo "Explain pre-reqs for understanding other typed systems")))
+
+(slide #:title "Atkey (2014)"
+   (para (todo "point in space. point in potential field.")))
+
+(slide #:title "Atkey (2014)"
+   (para (todo "nbody")))
+
+;;  In case we have extra time
+(slide #:title "Atkey (2014)"
+   (para (todo "pendulum")))
+(slide #:title "Atkey (2014)"
+   (para (todo "oscillator")))
+
+(slide #:title "Atkey (2014)"
+   (para "The rest of the paper formalizes this type system. You should read it."))
